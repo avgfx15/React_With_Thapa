6f549aac80a7ad9c8e7073538ba6f07e4bd89d6a
avgfx15 <avgfx15@gmail.com>
2025-08-10 14:52:17 +0530

    Login Form With UseState

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import UseStateArrayComponent from "./components/HOOKS/UseStateArrayComponent";
 import LiftingState from "./components/HOOKS/LiftingStateComponent/LiftingState";
 import CountChallange from "./components/HOOKS/UseStateHookComponent/CountChallange";
 import RegistrationFormComponent from "./components/HOOKS/UseStateHookComponent/RegistrationFormComponent";
+import LoginFormComponent from "./components/HOOKS/UseStateHookComponent/LoginFormComponent";
 
 const App = () => {
   return (
     <>
-      <RegistrationFormComponent />
+      <LoginFormComponent />
+      {/* <RegistrationFormComponent /> */}
       {/* <CountChallange /> */}
       {/* <h1 className="text-4xl font-bold underline text-center mt-5 text-blue-500">
         Hello world!
--- src/components/HOOKS/UseStateHookComponent/LoginFormComponent.jsx
+++ src/components/HOOKS/UseStateHookComponent/LoginFormComponent.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { useState } from "react";
 
-const RegistrationFormComponent = () => {
+const LoginFormComponent = () => {
   // @ State Variable
   const [inputFormData, setInputFormData] = useState({
-    name: "",
     username: "",
     email: "",
-    mobile: "",
     password: "",
   });
 
@@ -28,32 +26,15 @@ const RegistrationFormComponent = () => {
 
   // @ Render Component
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-300 shadow">
-      <h1 className="text-4xl font-bold text-center mt-5 text-blue-500">
-        Registration Form Component
+    <div className="flex flex-col justify-center items-center h-screen bg-blue-300">
+      <h1 className="text-4xl font-bold text-center mt-5 text-blue-500 mb-5">
+        RegistrationFormComponent
       </h1>
       <form
-        className="flex flex-col items-center justify-center gap-4 w-3/5"
+        className="flex flex-col items-center justify-center gap-4 w-3/5 bg-gray-300 p-5 rounded-lg shadow-2xl"
         onSubmit={handleSubmit}
       >
         <div className="my-4 w-full">
-          <label
-            className="block text-gray-700 text-lg font-bold mb-2"
-            htmlFor="email"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Please Enter Your Name"
-            value={inputFormData.name}
-            onChange={handleChange}
-            className="border-2 border-black-300 rounded-lg p-2 w-full"
-          />
-        </div>
-        <div className="mb-4 w-full">
           <label
             className="block text-gray-700 text-lg font-bold mb-2"
             htmlFor="username"
@@ -87,23 +68,7 @@ const RegistrationFormComponent = () => {
             className="border-2 border-black-300 rounded-lg p-2 w-full"
           />
         </div>
-        <div className="mb-4 w-full">
-          <label
-            className="block text-gray-700 text-lg font-bold mb-2"
-            htmlFor="mobile"
-          >
-            Mobile
-          </label>
-          <input
-            type="text"
-            name="mobile"
-            id="mobile"
-            placeholder="Please Enter Your Mobile 1234567890"
-            value={inputFormData.mobile}
-            onChange={handleChange}
-            className="border-2 border-black-300 rounded-lg p-2 w-full"
-          />
-        </div>
+
         <div className="mb-4 w-full">
           <label
             className="block text-gray-700 text-lg font-bold mb-2"
@@ -126,7 +91,7 @@ const RegistrationFormComponent = () => {
             type="submit"
             className="bg-blue-500 text-white p-3 rounded-lg text-xl"
           >
-            Submit
+            Login
           </button>
         </div>
       </form>
@@ -134,4 +99,4 @@ const RegistrationFormComponent = () => {
   );
 };
 
-export default RegistrationFormComponent;
+export default LoginFormComponent;
